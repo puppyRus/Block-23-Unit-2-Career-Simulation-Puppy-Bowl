6988676ae188b73e96328e872e750f0274f05453
Pedro Perez <pipr96@gmail.com>
2023-06-09 22:40:33 -0700

    Modify and fix play function

--- puppy-bowl.js
+++ puppy-bowl.js
@@ -1,5 +1,5 @@
 //
-const miniGame = document.querySelector('#teams-container') 
+const teamsContainer = document.querySelector('#teams-container') 
 
 //sets score to the players
 const getScore = () =>{ return Math.floor(Math.random()*10+2)};
@@ -14,12 +14,18 @@ const fetchAllTeams = async () =>{
     } catch (error) {
         console.error(error)
     }
-    console.log("Teams:", teams);
 }
 
 //render all teams
 const renderAllTeams = (teams)=>{
     console.log("teams:",teams)
+    //
+    const teamsTitle = document.createElement("h2");
+    teamsTitle.innerText = "Teams"
+    teamsContainer.appendChild(teamsTitle);
+    const miniGame = document.createElement("div");
+    miniGame.setAttribute("id","mini-game")
+    teamsContainer.appendChild(miniGame);
     //loop thru each team and get their name, id, players
     teams.forEach( e =>{
         console.log("team name:", e.name);    
@@ -43,37 +49,50 @@ const renderAllTeams = (teams)=>{
             player.innerText = `${element.name} - ${element.status}`;      
             playerList.appendChild(player);      
         });
-
     })
+    const startButton = document.createElement("button");
+    startButton.innerText = "Start Bowl"
+    startButton.setAttribute("onclick",`play()`)
+    teamsContainer.appendChild(startButton);
 }
 
-
-
 //play 
-const play = (team) =>{
-    //
-    let score = 0;//set score to 0
-    let players = [];//keep track of individual points
-    team.forEach(element => {
-        if (element.status == "field"){//check which teammates are going to play
-            let points = getScore();//invoke function to get score
-            console.log(`${element.name} gets ${points}`);
-            players.push(points);//add points to each player
-            score += points; //keep track of the score
-            //console.log("Score:",score);
+const play = async () =>{
+    //fetch teams
+    const response = await fetchAllTeams();//fet response
+    const teams = response.data.teams;//get teams from data response
+    //teams
+    const totalScores = []
+    teams.forEach(e =>{
+        //Create variables
+        let team = e.players; //get players from team to play 
+        let score = 0;//set score to 0
+        let players = [];//keep track of individual points
+        console.log("team",team)
+        team.forEach(element => {
+            if (element.status == "field"){//check which teammates are going to play
+                let points = getScore();//invoke function to get score
+                console.log(`${element.name} gets ${points}`);
+                players.push(points);//add points to each player
+                score += points; //keep track of the score
+                //console.log("Score:",score);
+            }
+        });
+        console.log("Total score",score);
+        console.log(players);
+        totalScores.push(score);
+        //Case scenarios
+        if(score === 60){
+            console.log("Perfect score, your Puppies are awesome")
+        }else if(score < 60 && score > 25){
+            console.log("Good work, overall performance from your puppies");
+        }else{
+            console.log("Low Performance");
         }
-    });
-    console.log("Total score",score);
-    console.log(players);
-    //Case scenarios
-    if(score === 60){
-        console.log("Perfect score, your Puppies are awesome")
-    }else if(score < 60 && score > 25){
-        console.log("Good work, overall performance from your puppies");
-    }else{
-        console.log("Low Performance");
-    }
-    return score;
+        return score;
+    });//changes team
+    console.log("Scores",totalScores)
+    totalScores[0] > totalScores[1] ? alert(`Team 1 won with ${totalScores[0]} points`) : alert(`Team 2 won with ${totalScores[1]}`); // check who won
 }
 
 
@@ -85,16 +104,9 @@ const init = async ()=>{
     
     //render teams
     renderAllTeams(teams);
-
-    //set teams
-    const team1 = teams[0].players; //add players to teams
-    const team2 = teams[1].players; //add players to team
-    //console.log("Eqipos:", team1, team2);
     
     //play
-    const scoreTeam1 = play(team1);
-    const scoreTeam2 = play(team2);
-    scoreTeam1 > scoreTeam2 ? console.log("Team 1 won") : console.log("Team 2 won"); // check who won
+   
     
 }
 
